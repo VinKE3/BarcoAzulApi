53bf4b4ae3bbc37dc9f0cde80eeabe002695d853
Kevin Muñoz <harquin09@gmail.com>
2024-12-13 17:04:23 -0500

    EmpezandoMovimientoArticulos

---
+++ BarcoAzul.Api.Modelos/Otros/Informes/oMovimientoArticulo.cs
@@ -0,0 +1,22 @@
+﻿namespace BarcoAzul.Api.Modelos.Entidades
+{
+    public class oMovimientoArticulo
+    {
+        public string LineaId { get; set; }
+        public string SubLineaId { get; set; }
+        public string ArticuloId { get; set; }
+        public string CodigoBarras { get; set; }
+        public string LineaDescripcion { get; set; }
+        public string SubLineaDescripcion { get; set; }
+        public string MarcaNombre { get; set; }
+        public string ArticuloDescripcion { get; set; }
+        public string UnidadMedidaAbreviatura { get; set; }
+        public decimal StockInicial { get; set; }
+        public decimal CantidadEntrada { get; set; }
+        public decimal CantidadSalida { get; set; }
+        public decimal SaldoFinal { get; set; }
+        public string TipoExistenciaId { get; set; }
+        public string TipoExistenciaDescripcion { get; set; }
+        public string MonedaId { get; set; }
+    }
+}
